fc774bada4696da6eccd9496957a40dc4fc2cf31
Michael Nekrasov <mikrasov@gmail.com>
2019-02-08 11:11:27 -0800

    First Attempt to make gatsby have hybrid properties

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+  siteMetadata: {
+    title: 'EmerGence',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'EmerGence- A Disaster Relief App',
+        short_name: 'EmerGence',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#c62828',
+        display: 'minimal-ui',
+        orientation: "portrait-primary",
+        description: "A simple photo and message sharing app with offline functionality.",
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: `emergence`,
+        server: { address: 'localhost', port: 27017 },
+        collection: [`posts`]
+      }
+    },
+    'gatsby-plugin-catch-links',
+    `gatsby-plugin-offline`
+  ],
+}
